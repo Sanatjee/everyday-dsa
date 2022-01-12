3daad8137ea7de10f6800b51f47642270e1b7f7d
Sanat Gawade <sanat.leo9@gmail.com>
2022-01-12 16:07:17 +0530

    implemented smallest number in an array

--- linear-search/src/enigma/LinearSearch.java
+++ linear-search/src/enigma/LinearSearch.java
@@ -25,17 +25,31 @@ class SearchAlgo{
         }
         return false;
     }
+
+    public static int findMinNumber(int[] arr){
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
 }
 
 public class LinearSearch extends SearchAlgo {
 
     public static void main(String[] args) {
         //Find wether 14 Exists in the array
-        int[] arr = {18,12,9,14,77,50};
+        int[] arr = {18,12,9,14,77,50,2};
         System.out.println(searchNumber(arr,14));
 
         String text = "SanatGawade";
         char target = 'Z';
         System.out.println(searchChar(text,target));
+
+        System.out.println("Smallest Number is : " + findMinNumber(arr));
     }
 }
