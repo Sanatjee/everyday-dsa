21656aae6a2036d67d5447b99e12f401ad3684d6
Sanat Gawade <sanat.leo9@gmail.com>
2022-01-12 15:43:45 +0530

    searching for char in a string implemented

--- linear-search/src/enigma/LinearSearch.java
+++ linear-search/src/enigma/LinearSearch.java
@@ -2,8 +2,24 @@ package enigma;
 
 class SearchAlgo{
     public static boolean searchNumber(int[] nums, int numToSearch){
+        if(nums.length == 0){
+            return false;
+        }
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == numToSearch){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchChar(String text, char target){
+        if(text.length() == 0){
+            return false;
+        }
+
+        for(int i = 0; i < text.length(); i++){
+            if(text.charAt(i) == target ){
                 return true;
             }
         }
@@ -17,5 +33,9 @@ public class LinearSearch extends SearchAlgo {
         //Find wether 14 Exists in the array
         int[] arr = {18,12,9,14,77,50};
         System.out.println(searchNumber(arr,14));
+
+        String text = "SanatGawade";
+        char target = 'Z';
+        System.out.println(searchChar(text,target));
     }
 }
