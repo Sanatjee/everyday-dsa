52cd7220b9bb52c564820d8b6285eabb2d358c8c
Sanat Gawade <sanat.leo9@gmail.com>
2022-01-12 19:38:19 +0530

    Linear Search Tut Completed

--- linear-search/src/enigma/LinearSearch.java
+++ linear-search/src/enigma/LinearSearch.java
@@ -1,5 +1,7 @@
 package enigma;
 
+import java.util.Arrays;
+
 class SearchAlgo{
     public static boolean searchNumber(int[] nums, int numToSearch){
         if(nums.length == 0){
@@ -37,6 +39,29 @@ class SearchAlgo{
 
         return min;
     }
+
+    public static int findMaxNumber(int[] arr){
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static int[] searchIn2DArray(int[][] arr, int target){
+        for(int row = 0; row < arr.length; row++){
+            for(int col = 0; col < arr[row].length; col++){
+                if(arr[row][col] == target){
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
 
 public class LinearSearch extends SearchAlgo {
@@ -50,6 +75,21 @@ public class LinearSearch extends SearchAlgo {
         char target = 'Z';
         System.out.println(searchChar(text,target));
 
+        //Finding smallest number
         System.out.println("Smallest Number is : " + findMinNumber(arr));
+
+        //Finding largest number
+        System.out.println("Largest Number is : " + findMaxNumber(arr));
+
+//        Find in 2D Array
+        int[][] TwoDArray = {
+                {24,2,3},
+                {9,65,4},
+                {78,6,3}
+        };
+        int TwoDTarget = 6;
+
+        int[] ans = searchIn2DArray(TwoDArray, TwoDTarget);
+        System.out.println(Arrays.toString(ans));
     }
 }
